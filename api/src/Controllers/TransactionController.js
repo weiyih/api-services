1630830fe0cd46d15990f7d7da73500a9cfe4bb4
Kevin <weiyih@sheridancollege.ca>
2020-06-03 11:35:40 -0400

    added blockchain transaction controller

--- api/src/Controllers/TransactionController.js
+++ api/src/Controllers/TransactionController.js
@@ -1,7 +1,8 @@
 // https://hyperledger.github.io/fabric-sdk-node/master/module-fabric-network.html
 
 const { Wallets, Gateway, Transaction, TimeoutError } = require('fabric-network');
-
+const fs = require('fs');
+const path = require('path');
 
 // Provides persisten reusable connection to peer within netowrk
 // Access to any (Network) channel peer is member of, which provides
@@ -9,41 +10,74 @@ const { Wallets, Gateway, Transaction, TimeoutError } = require('fabric-network'
 // Transaction submitted or queries evaluated
 
 class TransactionController {
+
+
+    // Check wallet for existing app identity
+    // TODO: Migrate to app registration/enrollment with CA for better security
+    // console.log(gateway);
+
+
+
+    // // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('voting-channel');
+    // // Get the contract from the network.
+    const contract = network.getContract('vote-contract');
+
+
+
     constructor() {
-        this.gateway = new Gateway(); // Gateway
+        this.gateway = new Gateway();; // Gateway
         this.network = null; // Network
         this.wallet = null; // Wallet
         this.contract = null; // Contract
+
+        const ccpPath = path.resolve(__dirname, '../src/config/org_profile.json');
+        const ccp = fs.readFileSync(ccpPath, 'utf8'); // Common Connection Profile
+        this.ccpJSON = JSON.parse(ccp)
+
     }
 
     async init() {
         this.setupWallet();
-
         this.setupGateway();
         this.setupNetwork();
         this.setupContract();
     }
 
     // Identity setup
-    setupWallet() {
-        this.wallet = new Wallets.newFileSystemWallet('path to wallet') // identity of application
-        // Wallet implementation
-        // const identity: X509Identity = {
-        //     credentials: {
-        //         certificate: 'PEM format certificate string',
-        //         privateKey: 'PEM format private key string',
-        //     },
-        //     mspId: 'wonderland',
-        //     type: 'X.509',
-        // };
-        // await wallet.put('alice', identity);
+    async setupWallet() {
+
+        const walletPath = path.resolve(__dirname, '../src/config/wallet');
+        thiswallet = new FileSystemWallet(walletPath);
+
+        const walletPath = path.resolve(__dirname, '../config/voting-api-client_identity.json');
+        this.wallet = new Wallets.newFileSystemWallet(walletPath) // identity of application
+
+        const identity = await wallet.exists('voting-api-client');
+        if (!identity) {
+            const cert = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/certificate.pem'));
+            const private = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/private_key.pem'));
+            let appIdentity = X509WalletMixin.createIdentity('voting-sheridan', cert, private);
+            await wallet.import('voting-api-client', appIdentity);
+        } else {
+            console.log('App identity exists');
+        }
+
+
+
     }
 
     async setupGateway() {
-        // TODO - Timeout retry
+        const gatewayOptions = {
+            identity: 'voting-api-client',
+            wallet,
+            discovery: { enabled: true, asLocalhost: false },
+        }
+
         try {
-            await this.gateway.connect(commonConnectionProfile, gatewayOptions)
-        } catch (error) {
+            await gateway.connect(ccpJSON, gatewayOptions);
+        }
+        catch (error) {
             console.log(error);
         }
     }
@@ -51,7 +85,7 @@ class TransactionController {
     async setupNetwork() {
         // TODO - Migrate to discovery service
         try {
-            const channelName = 'election-name';
+            const channelName = 'voting-channel';
             this.network = await this.gateway.getNetwork(channelName);
         } catch (error) {
             console.log(error);
@@ -75,12 +109,9 @@ class TransactionController {
 
     // Perform transaction
     // userVote : Vote object
-    async submitTransaction(userVote) {
-        let voteArgs = arg1, arg2, arg3
+    async submitTransaction(ballot) {
         try {
-            const transaction = "record";
-            // equivalent to createTransaction(name).submit()
-            let submitResults = await this.contract.submitTransaction(transaction, voteArgs);
+	        await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
             // TODO - 
             if (submitResults) {
                 console.log(submitResults);
@@ -101,4 +132,76 @@ class TransactionController {
             }
         }
     }
+
+    // Clean up and disconnect this Gateway connection
+	disconnect() {
+		this.gateway.disconnect();
+	}
+	
 }
+
+module.exports = TransactionController;
+
+
+// const UserDB = require('./controllers/UserDBController');
+
+
+// // UserDB.loadUser('222078a2-054e-4cc0-b8ae-c0693eadbafb')
+// // .then(res => {
+// //     console.log(res);
+// // })
+
+
+// const user = {
+// 	"first_name": "Test",
+// 	"middle_name": "",
+// 	"last_name": "Test",
+// 	"date_of_birth": "2001-01-31",
+// 	"email": "test@test.ca",
+// 	"device_id": "",
+// 	"driver_license": "W2247-79009-10131"
+// }
+
+// UserDB.createUser(user, 'placeholder').then(res => {
+//     // console.log(res);
+// })
+
+// https://hyperledger.github.io/fabric-sdk-node/master/module-fabric-network.html
+
+const { FileSystemWallet, Gateway, Transaction, TimeoutError, X509WalletMixin } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+
+async function main() {
+    try {
+        c
+
+        const ballot = {
+            'election_id': '9cd5f582-75e5-4bee-b451-e5417c18e761',
+            'ward': '1',
+            'selected_candidate': '2d8248ab-a831-4b5c-a3b2-6c5ef317731a',
+            'timestamp': String(Date.now()),
+        }
+
+        // for (let i = 2; i < 50; i++) {
+        // 	const key = 'VOTE' + i
+        // 	await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
+        // }
+
+        // await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
+        const result = await contract.submitTransaction('queryAllVotes', 'VOTE0');
+        // const result = await contract.submitTransaction('readVote', 'VOTE0');
+        console.log(result.toString());
+
+    } catch (error) {
+        if (error instanceof TimeoutError) {
+            // TODO: Recheck query
+        } else {
+            console.log(error);
+        }
+    }
+
+}
+
+main();
