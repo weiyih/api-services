70a116cf7cdb4197a130fa81a0c7a8088a798f2b
Kevin <weiyih@sheridancollege.ca>
2020-06-03 21:59:02 -0400

    fixed submit logic

--- api/src/app.js
+++ api/src/app.js
@@ -4,25 +4,32 @@ const express = require('express');
 const helmet = require('helmet'); // https://helmetjs.github.io
 const bodyParser = require('body-parser'); // https://github.com/expressjs/body-parser body parsing middleware
 const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session')
 const cors = require('cors'); // https://github.com/expressjs/cors CORS middleware
 const morgan = require('morgan'); // https://github.com/expressjs/morgan HTTP request logger middleware
-const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
-// const logger = require('morgan');
+const logger = require('morgan');
 const ElectionDB = require('./controllers/ElectionDBController');
 const UserDB = require('./controllers/UserDBController');
+const VoterDB = require('./controllers/VoterDBController');
+const Transaction = require('./controllers/TransactionController')
+
 require('dotenv').config();
 
+// TODO - docker secrets for pem files
 const {
   JWT_EXPIRY_SECOND,
   JWT_PRIVATE_KEYFILE,
 } = process.env;
 
 const JWT_PRIVATE_KEY = fs.readFileSync(
-  path.resolve(__dirname, "./config/ec_private.pem")
+  path.resolve(__dirname, JWT_PRIVATE_KEYFILE)
+);
+
+// TODO - REMOVE -> App will be using public key to encrypt
+const JWT_PUBLIC_KEY = fs.readFileSync(
+  path.resolve(__dirname, './config/ec_public.pem')
 );
 // const OpenApiValidator = require('express-openapi-validator').OpenApiValidator;
 
@@ -31,17 +38,38 @@ const JWT_PRIVATE_KEY = fs.readFileSync(
  * Express Settings
  */
 const app = express();
-// app.use(cookieSession({
-//   name: 'session',
-//   keys: ['token'],
-//   maxAge: JWT_EXPIRY_SECOND,
-// }))
 app.use(cookieParser());
 app.use(helmet()); // Helmet middleware to enforce HSTS
 app.use(bodyParser.json()); //parse JSON bodies into JS objects
 app.use(cors());
 app.use(morgan('combined'));
-// app.use(bodyParser.urlencoded({}))
+
+// Middleware for authenticating JWT
+const authenticateJWT = function (req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+
+    // Authorization header missing
+    if (authHeader) {
+      const authToken = authHeader.split(' ')[1];
+      // TODO replace with private key
+      const verified = jwt.verify(authToken, JWT_PUBLIC_KEY);
+      if (verified) {
+        req.verified = verified;
+        next();
+      } else {
+        return res.status(401).end();
+      }
+    } else {
+      return res.status(401).end();
+    }
+  } catch (error) {
+    return res.status(401).end();
+    // if (error instanceof jwt.JsonWebTokenError) {
+    //   return res.status(401).end();
+  }
+}
+
 
 
 /**
@@ -50,8 +78,11 @@ app.use(morgan('combined'));
  */
 app.post('/v1/signup', (req, res) => {
   const user = req.body
-
-  // TODO - Verify user credentials and match to voter DB
+  /*
+  * TODO 
+  * VerificationController
+  * Verify user credentials by matching VoterDB
+  */
   UserDB.createUser(user, 'voter-uuid-placeholder')
     .then((res) => {
       const response = {
@@ -64,7 +95,7 @@ app.post('/v1/signup', (req, res) => {
       res.send(response);
     }).catch(error => {
       const response = {
-        'status': true,
+        'status': false,
         'data': {
           'message': 'signup unsuccessful',
           'error': error
@@ -80,8 +111,7 @@ app.post('/v1/signup', (req, res) => {
 app.post('/v1/login', (req, res) => {
 
   // TODO Login with user credentials
-  const user = req.body;
-  console.log(user);
+  // const user = req.body;
 
   let response = {
     status: "success",
@@ -92,9 +122,10 @@ app.post('/v1/login', (req, res) => {
     JWT_PRIVATE_KEY,
     { expiresIn: JWT_EXPIRY_SECOND, algorithm: 'ES256' },
     function (err, token) {
-      if (err) { console.log(err) }
+      if (err) {
+        console.log(err)
+      }
       // Set cookie as token string and send
-      console.log(token);
       res.cookie('token', token, { maxAge: JWT_EXPIRY_SECOND });
       res.send(response);
     });
@@ -105,10 +136,8 @@ app.post('/v1/login', (req, res) => {
  * GET REQUEST
  * Response: Election JSON object
  */
-app.get('/v1/election', (req, res) => {
+app.get('/v1/election', authenticateJWT, (req, res) => {
   const data = ElectionDB.electionData;
-  // console.log(data);
-  // TODO - PKI encrypt data
   res.json(data)
 });
 
@@ -117,119 +146,154 @@ app.get('/v1/election', (req, res) => {
  * GET REQUEST
  * Response: Ballot JSON object
  */
+// app.get('/v1/ballot', authenticateJWT, (req, res) => {
 app.get('/v1/ballot', (req, res) => {
   const data = ElectionDB.ballotData;
   res.json(data);
 });
 
+
 /**
  * POST REQUEST
  * Submit ballot
  */
-app.post('/v1/submit', (req, res) => {
+  app.post('/v1/submit', authenticateJWT, function (req, res) {
+  try {
+    // TODO - DIGITAL SIGNATURE VERIFICATION
+    const ballot = req.body;
 
-  // Validate JWT
-  // Get user_id
-  const user_id = '222078a2-054e-4cc0-b8ae-c0693eadbafb';
-
-  // const ballot = req.body;
-  // Validate ballot JWT from user
-  // const userPublicKey = UserDB.getPublicKey(user.user_id);
-  const ballot = {
-    'election_id': '9cd5f582-75e5-4bee-b451-e5417c18e761',
-    'ward': "1",
-    'selected_candidate': '2d8248ab-a831-4b5c-a3b2-6c5ef317731a',
-    'timestamp': Date().now(),
-  }
-
-  let response = {
-    "success": true,
-    "data": {
-      "message": ''
-    }
-  }
-  let suceess;
-  let message;
-
-  //Validate ballot before validating user 1
-  if (validateBallot(ballot)) {
-    // Load User
-    UserDB.loadUser(user_id)
-      // Load voter information and check voter status
-      .then(function (user) {
-        const voterId = user.voter_id;
-        // return VoterDB.getVoterStatus(voter_id);
-        // Temporary dummy data
-        return {
-          voter_id: '3b241101-e2bb-4255-8caf-4136c566a962',
-          ward: 1,
-          vote_status: 'No',
+    //Validate ballot before validating user
+    const validated = validateBallot(ballot);
+    // Vote not validated
+    if (!validated) {
+      const response = {
+        'status': false,
+        'data': {
+          'error': 'error',
+          'message': 'unable to submit vote',
         }
-      })
-      // Check voter vote status
-      // Yes - Notify user
-      // Pending - Notify user
-      // No - process vote
-      .then(function (voter) {
-        const voteStatus = voter.vote_status;
-        // TODO Create custom Error
-        if (voteStatus === 'Yes') {
-          throw new Error('user has voted');
-        }
-        if (voteStatus === 'Pending') {
-          throw new Error("vote pending");
-        }
-        return //Submit Vote
       }
-        .catch(error => {
-          suceess = false;
-          if (error.message == 'user has voted') {
-            message = 'user voted';
-          } else if (error.message == 'vote pending')
-            message = 'vote pending';
-        }));
-  }
+      res.send(response);
 
-  const vote = {
-    user_timestamp: ballot.timestamp,
-  }
+    } else {
+      //req.verified is set from authenticateJWT
+      const userId = req.verified.user_id;
+      let user;
+      let voter;
+      let voterId;
 
+      // Load User and Voter profiles
+      UserDB.loadUser(userId)
+        .then((userResult) => {
+          user = userResult;
+          voterId = user.voter_id;
+          return VoterDB.loadVoter(voterId);
+        })
+        .then((voterResult) => {
+
+          voter = voterResult;
+          const voteStatus = voter.vote_status;
+
+          // TODO: DISABLE CURRENTLY TO ALLOW TRANSACTIOn
+          // if (voteStatus == 'Yes') {
+          //   throw new Error('user has voted');
+          // }
+
+          // TODO - Pending Vote Status
+          // } else if (voteStatus === 'Pending') {
+          //   throw new Error("vote pending");
+          // }
+          return Transaction.submitTransaction(ballot);
+        })
+        .then((submitResult) => {
+          // TODO - Email receipt
+          if (submitResult == true) {
+            VoterDB.updateUserVoteStatus(voterId, "Yes");
+            const response = {
+              'status': true,
+              'data': {
+                'message': 'submitted',
+              }
+            }
+            res.send(response);
+            // } else {
+            //   VoterDB.updateUserVoteStatus(voterId, "No");
+          }
+        })
+        // TODO - Fix catch error centralize
+        .catch((error) => {
+          // throw new Error(error);
+          const response = {
+            'status': true,
+            'data': {
+              'error': 'error',
+              'message': error.message,
+            }
+          }
+          res.send(response);
+        })
+    }
+  } catch (error) {
+    console.log('error', error);
+  }
 });
 
-// Default route - 403
-app.use((req, res) => {
-  res.status(403).end()
-});
+// // Missing routes are forbidden
+// app.use((req, res, next) => {
+//   res.status(403).end()
+//   next();
+// });
 
+// TODO - Refactor to a seperate file
 // Check if ballot has incorrect data
-const validateBallot = (ballot) => {
-  // Invalid election id
+function validateBallot(ballot) {
+  // Invalid election id 
   if (ballot.election_id !== ElectionDB.electionData.election_id) {
     return false;
   }
 
+  // TODO - Ward/District Verification
+
   // Timestamp and election dates
-  const currentTime = Date().now()
-  if (currentTime - ballot.timestamp >= 30000) { return false; }
+  const currentTime = Date.now()
+  const ballotTime = Number(ballot.timestamp);
+  // TODO - REDUCE TIME
+  const SUBMISSION_LIMIT = 6000000; // 10 minutes
 
-  // Advanced polling enabled
-  if (ElectionDB.electionData.advanced_polling) {
-    if (
-      (currentTime < ElectionDB.electionData.advanced_start_date) ||
-      (currentTime > ElectionDB.electionData.advanced_end_date)
-    ) { return false; }
-  } else {
-    if (
-      (currentTime < ElectionDB.electionData.election_start_date) ||
-      (currentTime > ElectionDB.electionData.election_end_date)
-    ) { return false; }
-  }
-
-  // Check selected candidate is within ward
-  if (!ElectionDB.ballotData.candidate.include(ballot.selected_candidate)) {
+  // Prevent ballot from being submitted if submission timestamp is >= SUBMISSION_LIMIT
+  if (currentTime - ballotTime >= SUBMISSION_LIMIT) {
     return false;
   }
-  return true;
+
+  // Ensure Election allows for electronic voting
+  // Somewhat redundant but may be required in the future?
+  if (ElectionDB.electionData.advanced_polling) {
+    // Ensure voting period is within the advanced poll and election dates
+    const advStartDate = Date.parse(ElectionDB.electionData.advanced_start_date);
+    const advEndDate = Date.parse(ElectionDB.electionData.advanced_end_date);
+
+    const electionStartDate = Date.parse(ElectionDB.electionData.election_start_date);
+    const electionEndDate = Date.parse(ElectionDB.electionData.election_end_date);
+
+    if ((currentTime < advStartDate) || (currentTime >= advEndDate)) {
+      return false;
+    }
+    if ((currentTime < electionStartDate) || (currentTime > electionEndDate)) {
+      return false;
+    }
+
+    // Ensure candidate selected is part of the list of eligible candidates
+    const candidates = ElectionDB.ballotData.candidate
+
+    let candidateList = []
+    for (const [key, candidate] of Object.entries(candidates)) {
+      candidateList.push(candidate.candidate_id);
+    }
+    if (!candidateList.includes(ballot.selected_candidate)) {
+      return false;
+    }
+    return true;
+  }
 }
 
 module.exports = app;
