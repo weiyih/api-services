b1b438f759e0d44fc09e305eea30a4238d8cea3a
Kevin <weiyih@sheridancollege.ca>
2020-06-03 20:07:10 -0400

    blockchain controller

--- api/src/Controllers/TransactionController.js
+++ api/src/Controllers/TransactionController.js
@@ -1,6 +1,6 @@
 // https://hyperledger.github.io/fabric-sdk-node/master/module-fabric-network.html
 
-const { Wallets, Gateway, Transaction, TimeoutError } = require('fabric-network');
+const { FileSystemWallet, Gateway, TimeoutError, X509WalletMixin } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
@@ -9,109 +9,74 @@ const path = require('path');
 // access to Smart Contract
 // Transaction submitted or queries evaluated
 
+let key = 0;
+
 class TransactionController {
-
-
-    // Check wallet for existing app identity
-    // TODO: Migrate to app registration/enrollment with CA for better security
-    // console.log(gateway);
-
-
-
-    // // Get the network (channel) our contract is deployed to.
-    const network = await gateway.getNetwork('voting-channel');
-    // // Get the contract from the network.
-    const contract = network.getContract('vote-contract');
-
+    // TODO - Replace key with better index
 
 
     constructor() {
-        this.gateway = new Gateway();; // Gateway
+        this.gateway = new Gateway(); // Gateway
         this.network = null; // Network
         this.wallet = null; // Wallet
         this.contract = null; // Contract
 
-        const ccpPath = path.resolve(__dirname, '../src/config/org_profile.json');
-        const ccp = fs.readFileSync(ccpPath, 'utf8'); // Common Connection Profile
-        this.ccpJSON = JSON.parse(ccp)
-
     }
 
-    async init() {
-        this.setupWallet();
-        this.setupGateway();
-        this.setupNetwork();
-        this.setupContract();
-    }
-
-    // Identity setup
-    async setupWallet() {
-
-        const walletPath = path.resolve(__dirname, '../src/config/wallet');
-        thiswallet = new FileSystemWallet(walletPath);
-
-        const walletPath = path.resolve(__dirname, '../config/voting-api-client_identity.json');
-        this.wallet = new Wallets.newFileSystemWallet(walletPath) // identity of application
-
-        const identity = await wallet.exists('voting-api-client');
-        if (!identity) {
-            const cert = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/certificate.pem'));
-            const private = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/private_key.pem'));
-            let appIdentity = X509WalletMixin.createIdentity('voting-sheridan', cert, private);
-            await wallet.import('voting-api-client', appIdentity);
-        } else {
-            console.log('App identity exists');
-        }
-
-
-
-    }
-
-    async setupGateway() {
-        const gatewayOptions = {
-            identity: 'voting-api-client',
-            wallet,
-            discovery: { enabled: true, asLocalhost: false },
-        }
-
+    // Connect to blockchain network
+    async setup() {
         try {
-            await gateway.connect(ccpJSON, gatewayOptions);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
+            // Load App identity
+            const walletPath = path.resolve(__dirname, '../config/wallet');
+            this.wallet = new FileSystemWallet(walletPath);
 
-    async setupNetwork() {
-        // TODO - Migrate to discovery service
-        try {
-            const channelName = 'voting-channel';
-            this.network = await this.gateway.getNetwork(channelName);
+            const identity = await this.wallet.exists('voting-api-client');
+            // Create identity if it doesn't exist
+            // TODO - Migrate to registration service for security
+            if (!identity) {
+                const cert = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/certificate.pem'));
+                const privateKey = fs.readFileSync(path.resolve(__dirname, '../src/config/wallet/voting-api-client/private_key.pem'));
+                let appIdentity = X509WalletMixin.createIdentity('voting-sheridan', cert, privateKey);
+                await this.wallet.import('voting-api-client', appIdentity);
+            } else {
+                console.log('App identity exists');
+            }
+
+            // Common connection profile
+            const ccpPath = path.resolve(__dirname, '../config/org_profile.json');
+            const ccp = fs.readFileSync(ccpPath, 'utf8'); // Common Connection Profile
+            const ccpJSON = JSON.parse(ccp);
+
+            let wallet = this.wallet
+            const gatewayOptions = {
+                identity: 'voting-api-client',
+                wallet,
+                discovery: { enabled: true, asLocalhost: false },
+            }
+            // Connect to network
+            await this.gateway.connect(ccpJSON, gatewayOptions);
+            // Get the network (channel) our contract is deployed to.
+            this.network = await this.gateway.getNetwork('voting-channel');
+            // Get the contract from the network.
+            this.contract = this.network.getContract('vote-contract');
+
+            return;
         } catch (error) {
-            console.log(error);
-            throw new Error(error);
+            if (error instanceof TimeoutError) {
+                // TODO: Recheck query
+            } else {
+                console.log(error);
+            }
         }
     }
 
-    async setupContract() {
-        try {
-            const chaincode = 'election-name-contract';
-            this.contract = await this.network.getContract(chaincode);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    // Cleanup and diconnect gateway
-    disconnectGateway() {
-        this.gateway.disconnect();
-    }
-
-    // Perform transaction
+    // Submit vote as transaction
     // userVote : Vote object
     async submitTransaction(ballot) {
+        let voteKey = 'VOTE' + key;
+        this.key++;
         try {
-	        await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
+            let submitResults = await this.contract.submitTransaction('createVote', voteKey, ballot.election_id, ballot.ward, ballot.selected_candidate, ballot.timestamp);
             // TODO - 
             if (submitResults) {
                 console.log(submitResults);
@@ -122,86 +87,57 @@ class TransactionController {
                 // return { "result" : "failed"};
                 return false;
             }
-
         } catch (error) {
             // TimeoutError if the transaction was successfully submitted to the orderer but timed out before a commit event was received from peers. 
             if (error instanceof TimeoutError) {
                 // evaluate peers to see if transaction is on peers
-                let results = await this.contract.evaluateTransction(voteArgs)
-                console.log(results);
+                // TODO 
+                console.log(error);
             }
         }
     }
 
-    // Clean up and disconnect this Gateway connection
-	disconnect() {
-		this.gateway.disconnect();
-	}
-	
-}
-
-module.exports = TransactionController;
-
-
-// const UserDB = require('./controllers/UserDBController');
-
-
-// // UserDB.loadUser('222078a2-054e-4cc0-b8ae-c0693eadbafb')
-// // .then(res => {
-// //     console.log(res);
-// // })
-
-
-// const user = {
-// 	"first_name": "Test",
-// 	"middle_name": "",
-// 	"last_name": "Test",
-// 	"date_of_birth": "2001-01-31",
-// 	"email": "test@test.ca",
-// 	"device_id": "",
-// 	"driver_license": "W2247-79009-10131"
-// }
-
-// UserDB.createUser(user, 'placeholder').then(res => {
-//     // console.log(res);
-// })
-
-// https://hyperledger.github.io/fabric-sdk-node/master/module-fabric-network.html
-
-const { FileSystemWallet, Gateway, Transaction, TimeoutError, X509WalletMixin } = require('fabric-network');
-const fs = require('fs');
-const path = require('path');
-
-
-async function main() {
-    try {
-        c
-
-        const ballot = {
-            'election_id': '9cd5f582-75e5-4bee-b451-e5417c18e761',
-            'ward': '1',
-            'selected_candidate': '2d8248ab-a831-4b5c-a3b2-6c5ef317731a',
-            'timestamp': String(Date.now()),
-        }
-
-        // for (let i = 2; i < 50; i++) {
-        // 	const key = 'VOTE' + i
-        // 	await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
-        // }
-
-        // await contract.submitTransaction('createVote', key, ballot.election_id,  ballot.ward, ballot.selected_candidate, ballot.timestamp);
-        const result = await contract.submitTransaction('queryAllVotes', 'VOTE0');
-        // const result = await contract.submitTransaction('readVote', 'VOTE0');
-        console.log(result.toString());
-
-    } catch (error) {
-        if (error instanceof TimeoutError) {
-            // TODO: Recheck query
-        } else {
+    async queryAllVotes() {
+        try {
+            const result = await this.contract.submitTransaction('queryAllVotes');
+            // const result = await contract.submitTransaction('readVote', 'VOTE0');
+            // console.log(result.toString());
+            return (result.toString());
+        } catch (error) {
             console.log(error);
         }
     }
 
+    // Temp function to get/update VoteKey based on blockchain
+    async updateVoteKey() {
+        this.queryAllVotes()
+            .then(function (result) {
+                const resJSON = JSON.parse(result);
+                const votesLen = resJSON.length;
+                key = votesLen;
+                return;
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async readVote(voteKey) {
+        try {
+            // const result = await this.contract.submitTransaction('readVote', voteKey);
+            const result = await this.contract.submitTransaction('readVote', 'VOTE0');
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }
+
+    // Clean up and disconnect this Gateway connection
+    disconnect() {
+        this.gateway.disconnect();
+    }
+
 }
 
-main();
+module.exports = new TransactionController();
