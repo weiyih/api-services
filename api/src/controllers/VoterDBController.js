ea0322494b592b5353e9829aa25844fe165e91f8
Kevin <weiyih@sheridancollege.ca>
2020-11-30 22:37:19 -0500

    VoterDB to use Factory, refactored CRUD

--- api/src/controllers/VoterDBController.js
+++ api/src/controllers/VoterDBController.js
@@ -1,89 +1,96 @@
-const mongoose = require('mongoose');
-const voterSchema = require('../schemas/Voter');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const voterSchema = require("../schemas/Voter");
+require("dotenv").config();
 
-/**
- * Mongoose Schema
- * */
 let Voter;
 
-/**
- * Database Client Options and URI config
- */
-let CLIENT_OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Autoindex defined by schema great for development not production
-    poolSize: 5, // Default size is 5, only 1 operation per socket at a time
-    serverSelectionTimeoutMS: 5000, // Default is 30s
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    keepAlive: true,
-    keepAliveInitialDelay: 300000, // TCP KeepAlive every 30s
-    family: 4 // Use IPv4, skip trying IPv6
-};
-
-let {
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME,
-    MONGO_PORT,
-    VOTER_DB
-} = process.env;
-
-// const URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:${MONGO_PORT}/${USER_DB}?authSource=admin`
-const URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${VOTER_DB}?authSource=admin`
-
 class VoterDB {
-    constructor() {
-        this.conn = mongoose.createConnection(URI, CLIENT_OPTIONS);
-        this.conn.on('connected', () => {
-            // TODO - Notify app
-            console.log('Connected to Voter DB');
-        });
-        this.conn.on('error', (error) => {
-            console.log('Error', error);
-        });
+  constructor() {
+    const { VOTER_DB } = process.env;
 
-        //Register schema to connection
-        Voter = this.conn.model('voter', voterSchema, 'voter');
+    try {
+      const db = DBFactory.create(VOTER_DB);
+      // Buffer registering models
+      db.conn.on("connected", () => {
+        console.log(`Registering ${VOTER_DB} models`);
+        Voter = this.conn.model("voter", voterSchema, "voter");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // TODO - Refactor to return stream
+  async getVoter(req, res, next) {
+    const query = Voter.find().select("-_id -__v");
+    try {
+      const data = await query.exec();
+      res.json(data);
+    } catch (error) {
+      // TODO - handle error
+      console.log(error);
+      return res.status(500).send({ message: error.message });
+    }
+  }
+
+  // Retrieves the voter based on the voter uuid
+  async loadVoter(voterId) {
+    const query = Voter.findOne()
+      .where("voter_id")
+      .equals(voterId)
+      .select("-_id -__v");
+    try {
+      const data = await query.exec();
+      res.json(data);
+    } catch (error) {
+      // TODO - handle error
+      console.log(error);
+      return res.status(500).send({ message: error.message });
+    }
+  }
+
+  // Updates Voter document of the voting status
+  async updateUserVoteStatus(voterId, status) {
+    const query = Voter.findOne()
+      .where("voter_id")
+      .equals(voterId)
+      .select("-_id -__v");
+    try {
+      const data = await query.exec();
+      res.json(data);
+    } catch (error) {
+      // TODO - handle error
+      console.log(error);
+      return res.status(500).send({ message: error.message });
     }
 
-    // Retrieves the voter based on the voter uuid
-    async loadVoter(voterId) {
-        const query = Voter.findOne({ voter_id: voterId });
-        const res = await query.exec();
+    const query = Voter.findOneAndUpdate(
+      { voter_id: voterId },
+      { vote_status: status },
+      { new: true }
+    );
+    const res = await query.exec();
 
-        if (!res) {
-            return null;
-        }
-        return res;
+    if (!res) {
+      return null;
     }
+    return res;
+  }
 
-    // Updates Voter document of the voting status
-    async updateUserVoteStatus(voterId, status) {
-        const query = Voter.findOneAndUpdate({ voter_id: voterId }, { vote_status: status }, { new: true });
-        const res = await query.exec();
+  // Updates Voter document to reflect the User intention to vote online when registering
+  async updateUserOnlineVote(voterId) {
+    const query = Voter.findOneAndUpdate(
+      { voter_id: voterId },
+      { vote_online: "Yes" },
+      { new: true }
+    );
+    const res = await query.exec();
 
-        if (!res) {
-            return null;
-        }
-        return res;
+    if (!res) {
+      return null;
     }
-
-    // Updates Voter document to reflect the User intention to vote online when registering 
-    async updateUserOnlineVote(voterId) {
-        const query = Voter.findOneAndUpdate({ voter_id: voterId }, { vote_online: 'Yes' }, { new: true });
-        const res = await query.exec();
-
-        if (!res) {
-            return null;
-        }
-        return res;
-    }
-
-
+    return res;
+  }
 }
 
 module.exports = new VoterDB();
