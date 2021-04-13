51afee35de4528d6a4338f4681c027b935cd24c5
Kevin <weiyih@sheridancollege.ca>
2021-04-13 16:21:30 -0400

    refactor getElection and getBallot

--- voting-api/src/controllers/ElectionDBController.js
+++ voting-api/src/controllers/ElectionDBController.js
@@ -31,8 +31,8 @@ class ElectionDB {
     // Query retrieves all elections that are not disabled (ie. deleted)
     // Strips away objectId(_id) and document version(__v)
     const query = Election.find()
-      .where("disabled").equals(1) 
-      .select("-_id -__v"); 
+      .where("disabled").equals(1)
+      .select("-_id -__v");
     try {
       const data = await query.exec();
       res.json(data);
@@ -44,71 +44,36 @@ class ElectionDB {
     }
   }
 
-  async getElection(req, res, next) {
-    const electionId = req.params.id;
-    // Build query
-    const query = Election.findOne()
-      .where("election_id")
-      .equals(electionId)
-      .select("-_id -__v"); //Strips objectId(_id) and document version(__v)
+  /**
+   * Retrieve election by electionId
+   */
+  async getElection(electionId) {
     try {
-      const data = await query.exec();
+      const query = Election.findOne()
+        .where("election_id").equals(electionId)
+        .select("-_id -__v"); //Strips objectId(_id) and document version(__v)
 
-      res.json(data);
-      // next();
+      const data = await query.exec();
+      return data;
     } catch (error) {
-      // TODO - handle error
       console.log(error);
-      return res.status(500).send({ message: error.message });
+      throw (`Error - Unable to retrieve election: ${electionId}`)
     }
+
   }
 
-
-  async getCandidates(req, res, next) {
-    //TODO - Load user
-
-    const electionId = req.params.id;
+  async getBallot(electionId, districtId) {
     // Build query
     const query = Ballot.findOne()
-      .where("election_id")
-      .equals(electionId)
+      .where("election_id").equals(electionId)
+      .where("district_id").equals(districtId)
       .select("-_id -__v"); //Strips objectId(_id) and document version(__v)
-    try {
-      const data = await query.exec();
 
-      res.json(data);
-      // next();
-    } catch (error) {
-      // TODO - handle error
-      console.log(error);
-      return res.status(500).send({ message: error.message });
-    }
-  }
-
-  // /**
-  //  * Retrieves ballots based on electionId
-  //  * @param {*} electionId
-  //  * TODO - Refactor to allow for multiple elections and multiple wards
-  //  */
-  // async getBallots(electionId) {
-  async getBallots(req, res, next) {
-    const query = Ballot.findOne({ election_id: electionId });
-    res.json;
-    try {
-      const res = await query.exec();
-
-      const jsonData = JSON.stringify(data, (key, value) => {
-        if (key === "_id" || key === "__v") {
-          return undefined;
-        }
-        return value;
-      });
-      res.json(JSON.parse(jsonData));
-    } catch (error) {
-      // TODO - handle error
-      console.log(error);
-      return res.status(500).send({ message: error.message });
-    }
+    const data = await query.exec();
+    return data;
+  } catch(error) {
+    console.log(error);
+    throw ("Error - Unable to retrieve ballot")
   }
 }
 
