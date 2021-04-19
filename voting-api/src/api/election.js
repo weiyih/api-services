ae5ed6d441cd091f0f39535e74c796b0a4488b0f
Kevin <weiyih@sheridancollege.ca>
2021-04-18 23:30:49 -0400

    load election middleware

---
+++ voting-api/src/api/election.js
@@ -0,0 +1,18 @@
+const ElectionDB = require("../controllers/ElectionDBController");
+
+async function loadElection(req, res, next) {
+    const electionId = req.params.id
+    try {
+        if (!electionId) {
+            throw ("Error - Missing election id")
+        }
+        const election = await ElectionDB.getElection(electionId);
+        req.electionData = election;
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ message: error.message });
+    }
+}
+
+module.exports = { loadElection }
