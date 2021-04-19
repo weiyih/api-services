0e2febc38883ada6d10c1f6cbe403750fae36830
Kevin <weiyih@sheridancollege.ca>
2021-04-19 02:12:23 -0400

    wip getVoteStatus

--- voting-api/src/api/voter.js
+++ voting-api/src/api/voter.js
@@ -2,11 +2,21 @@ const VoterDB = require("../controllers/VoterDBController");
 
 async function updateVoteStatus(electionId, ballotId, status) {
     try {
-        const data = await VoterDB.updateUserVoteStatus(electionId, ballotId, status)
+        const data = await VoterDB.updateVoteStatus(electionId, ballotId, status)
         if (data) return true;
     } catch (error) {
         console.log(error)
     }
 }
 
-module.exports = updateVoteStatus
+async function getVoteStatus(electionId, ballotId, status) {
+    try {
+        const data = await VoterDB.getVoteStatus(electionId, ballotId, status)
+        if (data) return true;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+module.exports = { updateVoteStatus, getVoteStatus }
