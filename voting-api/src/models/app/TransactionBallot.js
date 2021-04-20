d44c8486088fb08100b7c8f1f1b7854ae12f03f9
Kevin <weiyih@sheridancollege.ca>
2021-04-20 12:07:11 -0400

    comment

--- voting-api/src/models/app/TransactionBallot.js
+++ voting-api/src/models/app/TransactionBallot.js
@@ -4,8 +4,13 @@ class TransactionBallot {
         this.election_id = electionId; //string
         this.district_id = districtId; // string
         this.candidate_id = candidateId; // string
-        this.timestamp = time // String
+        this.timestamp = time // Number
     }
 }
+
+// TODO - Along with all other app schemas
+// Use aggregate and virtual and toJSON functions to create the app response
+
+// Might be better to create an model interface for this and just enforce the data object to match it
 
 module.exports = TransactionBallot;
