c0510bd6668b96252b938bc4a4825d8c125127a8
Kevin <weiyih@sheridancollege.ca>
2021-04-20 12:04:57 -0400

    removed uncessary models. should replace with interface in future

--- voting-api/src/models/app/AppCandidate.js
+++
@@ -1,8 +0,0 @@
-class AppCandidate {
-    constructor(candidateId, candidateName) {
-        this.candidate_id = candidateId;
-        this.candidate_name = candidateName;
-    }
-}
-
-module.exports = AppCandidate;
