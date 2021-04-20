3a2f2c7dcce6974a39a8100c64d3483e6f600b61
Kevin <weiyih@sheridancollege.ca>
2021-04-20 11:19:22 -0400

    submit ballot

--- voting-api/src/controllers/TransactionController.js
+++ voting-api/src/controllers/TransactionController.js
@@ -8,44 +8,61 @@ const { connectGateway } = require('./network');
 // Access to any (Network) channel peer is member of, which provides
 // access to Smart Contract
 
-let gateway;
+function prettyJSONString(inputString) {
+    return JSON.parse(inputString);
+}
 
 // Transaction submitted or queries evaluated
 
-// Submit vote as transaction
-async function submitTransaction(ballot, channel) {
+async function queryBallot(voter, channel) {
     try {
-        gateway = await connectGateway()
+        const gateway = await connectGateway()
         const network = await gateway.getNetwork(channel)
         const contract = network.getContract('ballot-contract');
-        // Evaulate transaction on peers before submitted to the ordering service for commit to ledger
 
-        console.log(ballot);
-        
-        let submitResult = await contract.submitTransaction('CreateBallot',
-            [ ballot.voter_id,
-            ballot.election_id,
-            ballot.district_id,
-            ballot.candidate_id,
-            ballot.timestamp ]
-        )
-        // Ensure ballot can be queried
-        // let result = await contract.evaluateTransaction('ReadBallot', ballot.voter_id);
-        return submitResult;
-        // // ContractListener
-        // const listener = async (event) => {
-        //     console.log('chaincodeId', event.chaincodeId.toString())
-        //     console.log('eventName:', event.eventName.toString())
-        //     console.log('payload:', event.paylod.toString('utf8'))
-        //     // if (event.eventName === 'newOrder') {
-        //     //     const details = event.payload.toString('utf8');
-        //     //     // Run business process to handle orders
-        //     // }
-        //     contract.removeContractListner(listener);
-        // }
-        // contract.addContractListener(listener);
+        let result = await contract.evaluateTransaction('ReadBallot', ballot.voter_id);
+        console.log(`Query: ${prettyJSONString(submitResult.toString())}`)
+
     } catch (error) {
         console.log(error)
+    } finally {
+        disconnect()
+    }
+}
+
+
+// Submit vote as transaction
+async function submitBallotTransaction(ballot, channel) {
+    const gateway = await connectGateway()
+    const network = await gateway.getNetwork(channel)
+    const contract = network.getContract('ballot-contract');
+
+    // Parse out ballot information
+    const voter_id = ballot.id
+    const elec_id = ballot.election_id
+    const dist_id = ballot.district_id
+    const cand_id = ballot.candidate_id
+    const timestamp = ballot.timestamp
+
+    try {
+        // submitTransaction - Evaulates transaction on peers before submitting to the ordering service for commit to ledger
+        // NOTES: Assumes all votes submitted will be unique and successful
+        // TODO - Refactor to handle TimeoutErrors, and parse out blockchain errors
+        // Blockchain errors include unable to read world state, or ballot already exists
+        // Requires refactoring of chaincode and research on the best strategy to parse out errors as it is a string message
+        await contract.submitTransaction('CreateBallot', voter_id, elec_id, dist_id, cand_id, timestamp)
+		let query = await contract.evaluateTransaction('ReadBallot',voter_id);
+        const result = prettyJSONString(query.toString())
+        return result;
+    } catch (error) {
+        if (error.message == 'the ballot does not exist' ) {
+            throw Error('ballot exists')
+        } else {
+            console.log(error)
+            throw Error('unable to process ballot')  
+        }      
+    } finally {
+        gateway.disconnect()
     }
 }
 
@@ -60,10 +77,4 @@ async function submitTransaction(ballot, channel) {
 //     }
 // }
 
-// Clean up and disconnect this Gateway connection
-function disconnect() {
-    gateway.disconnect();
-}
-
-
-module.exports = { submitTransaction }; 
+module.exports = { submitBallotTransaction };
